21fef822ae163837072e893a63e9c4f7f4750330
Alex Morales <44649358+msalemor@users.noreply.github.com>
2022-02-23 19:54:44 -0500

    Update azclidacrbuild.sh

--- src/azclidacrbuild.sh
+++ src/azclidacrbuild.sh
@@ -9,11 +9,13 @@ acrname=alemoracr
 
 az acr build -r $acrname -f ./Sender.Dockerfile -t gosender:$version .
 az acr build -r $acrname -f ./Receiver.Dockerfile -t goreceiver:$version .
-az acr build -r $acrname -f ./EventsApi.Dockerfile -t goreceiver:$version .
-az acr build -r $acrname -f ./Monitor.Dockerfile -t goreceiver:$version .
+az acr build -r $acrname -f ./EventsApi.Dockerfile -t goeventsapi:$version .
+az acr build -r $acrname -f ./Monitor.Dockerfile -t gomonitor:$version .
 
 # List image attributes 
 #az acr repository list -n alemoracr
 
 az acr repository show -n alemoracr --image gosender:$version
 az acr repository show -n alemoracr --image goreceiver:$version
+az acr repository show -n alemoracr --image goeventsapi:$version
+az acr repository show -n alemoracr --image goeventsapi:$version
